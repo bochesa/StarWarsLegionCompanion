8c88a495ee7e5940eae121250ae0b9c79bc6b082
Henrik Levin Bochesa <henr15r4@edu.ucl.dk>
2022-01-02 21:57:55 +0100

    minor fix on DTOServices

--- ModelsLibrary/Application/Services/DTOServices.cs
+++ ModelsLibrary/Application/Services/DTOServices.cs
@@ -9,20 +9,6 @@ using UtilityLibrary.Models;
 
 namespace UtilityLibrary.Application.Services
 {
-    public interface IDTOServices
-    {
-        ICollection<OutUpgradeOptionDTO> CreateListOfOutUpgradeOptionsDTO(ICollection<UpgradeOption> unitUpgradeOptions);
-        ICollection<OutKeywordDTO> CreateListOfOutKeywordsDTO(ICollection<Keyword> requestKeywords);
-        ICollection<OutWeaponDTO> CreateListOfOutWeaponsDTO(ICollection<Weapon> requestWeapons);
-        OutWeaponDTO CreateOutWeaponsDTO(Weapon weapon);
-        ICollection<UpgradeOption> PostListOfUpgradeOptions(IEnumerable<InUpgradeOptionDTO> requestUpgradeOptions);
-        Task<ICollection<Keyword>> PostListOfKeywords(IEnumerable<InKeywordDTO> requestKeywords);
-        Task<ICollection<Weapon>> PostListOfWeapons(IEnumerable<InWeaponDTO> requestWeapons);
-        Task<Weapon> PostWeapon(InWeaponDTO requestWeapon);
-        ICollection<OutCommandDTO> GetCommandsForArmy(IEnumerable<ChosenCommand> armyChosenCommands);
-        ICollection<OutUpgradeDTO> GetChosenUpgradesForUnit(IEnumerable<ChosenUpgrade> unitChosenUpgrades);
-        ICollection<OutUnitDTO> GetChosenUnitsForArmy(IEnumerable<ChosenUnit> armyChosenUnits);
-    }
     public class DTOServices : IDTOServices
     {
         private readonly IUnitOfWork _uow;
---
+++ ModelsLibrary/Application/Services/IDTOServices.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UtilityLibrary.Application.Handlers;
+using UtilityLibrary.Models;
+
+namespace UtilityLibrary.Application.Services
+{
+    public interface IDTOServices
+    {
+        ICollection<OutUpgradeOptionDTO> CreateListOfOutUpgradeOptionsDTO(ICollection<UpgradeOption> unitUpgradeOptions);
+        ICollection<OutKeywordDTO> CreateListOfOutKeywordsDTO(ICollection<Keyword> requestKeywords);
+        ICollection<OutWeaponDTO> CreateListOfOutWeaponsDTO(ICollection<Weapon> requestWeapons);
+        OutWeaponDTO CreateOutWeaponsDTO(Weapon weapon);
+        ICollection<UpgradeOption> PostListOfUpgradeOptions(IEnumerable<InUpgradeOptionDTO> requestUpgradeOptions);
+        Task<ICollection<Keyword>> PostListOfKeywords(IEnumerable<InKeywordDTO> requestKeywords);
+        Task<ICollection<Weapon>> PostListOfWeapons(IEnumerable<InWeaponDTO> requestWeapons);
+        Task<Weapon> PostWeapon(InWeaponDTO requestWeapon);
+        ICollection<OutCommandDTO> GetCommandsForArmy(IEnumerable<ChosenCommand> armyChosenCommands);
+        ICollection<OutUpgradeDTO> GetChosenUpgradesForUnit(IEnumerable<ChosenUpgrade> unitChosenUpgrades);
+        ICollection<OutUnitDTO> GetChosenUnitsForArmy(IEnumerable<ChosenUnit> armyChosenUnits);
+    }
+}
